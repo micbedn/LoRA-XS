c9a7083259054242c2b0512313ab62e42d255b07
micbedn <bowlers_09cows@icloud.com>
2024-08-05 11:18:36 +0200

    .

--- main_glue.py
+++ main_glue.py
@@ -285,7 +285,7 @@ def main():
         lora_dropout=0.0,
         #target_modules=["query", "value", "attention.output.dense", "output.dense"],
         #target_modules=["query"],
-        target_modules=["layer.0.attention.self.query", "layer.1.attention.self.query"],
+        target_modules=["layer.0.attention.self.query"],
         #target_modules=["layer.23.attention.self.query"],
     )
 
